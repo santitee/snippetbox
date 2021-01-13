29ab1918e9bf17a5a5c3940a1aab29c9daa35992
santitee <santitee@gmail.com>
2021-01-13 14:02:37 +0700

    Create Template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+		return
+	}
+	files := []string {
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+	// w.Write([]byte("Hello from Snippetbox"))
+
 }
 
 // Add a showSnippet hanler function.
